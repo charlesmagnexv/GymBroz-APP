4d5b6f72fa69aa5559b348ef6fcae4cf7e60e30e
AdrianFSSoares <adriancurso2018@gmail.com>
2023-11-22 11:09:17 -0300

    Criação do ModalSignUp para cadastro de usuário (erro na comparação de senha com confirma senha) e criação da tela Conta mais estilizada (modal de trocar imagem de perfil sem conteudo e exemplo de lista de amigos ainda a fazer)

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -10,12 +10,14 @@ import {
     Spinner,
     View,
     Image,
-    Text
+    Text,
+    ScrollView
 } from 'native-base';
 import { useForm, Controller } from 'react-hook-form';
 import gymbrozTheme from "../../theme/gymbrozTheme";
 import { useFeedback } from "../../context/useFeedback";
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import ModalSignUp from "../../components/ModalSignUp";
 
 type FormData = {
     email: string;
@@ -40,10 +42,12 @@ const styles = StyleSheet.create({
         borderRadius: 5,
     },
 });
+  
 
 const SignIn: React.FC = () => {
-    const [loading, setLoading] = useState<boolean>(false)
+    const [loading, setLoading] = useState<boolean>(false);
     const [showPassword, setShowPassword] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
 
     const { signIn } = useAuth();
     const { control, handleSubmit, formState: { errors } } = useForm<FormData>();
@@ -70,107 +74,115 @@ const SignIn: React.FC = () => {
         })
     };
 
-    return (
-        <Center flex={1} justifyContent='flex-start' paddingTop={12}>
-            <Text bold fontSize="2xl" style={styles.title}>GymBroz</Text>
-            <Image source={require('../../../assets/img/img-gymbroz.png')} size={'xl'} alt={"Homem forte"} />
-            <VStack width="100%" mx="3" maxW="300px">
-                <FormControl isInvalid={'email' in errors}>
-                    <FormControl.Label mt={2}>E-mail</FormControl.Label>
-                    <Controller
-                        control={control}
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <Input
-                                onBlur={onBlur}
-                                placeholder="E-mail"
-                                onChangeText={(val) => onChange(val)}
-                                value={value}
-                                _focus={{
-                                    bg: 'lightBlue.50'
-                                }}
-                                autoCapitalize='none'
-                            />
-                        )}
-                        name="email"
-                        rules={{
-                            required: 'Campo obrigatório',
-                            pattern: { value: REGEX_EMAIL, message: 'E-mail inválido' }
-                        }}
-                        defaultValue=""
-                    />
-                    <FormControl.ErrorMessage>
-                        {errors.email?.message}
-                    </FormControl.ErrorMessage>
-                </FormControl>
-                <FormControl isInvalid={'password' in errors}>
-                    <FormControl.Label mt={2}>Senha</FormControl.Label>
-                    <Controller
-                        control={control}
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <Input
-                                onBlur={onBlur}
-                                secureTextEntry={!showPassword}
-                                placeholder="Password"
-                                type="password"
-                                onChangeText={(val) => onChange(val)}
-                                value={value}
-                                _focus={{
-                                    bg: 'lightBlue.50'
-                                }}
-                                autoCapitalize='none'
-                                InputRightElement={<MaterialCommunityIcons
-                                    name={showPassword ? 'eye-off' : 'eye'}
-                                    size={28}
-                                    color="#aaa"
-                                    style={styles.icon}
-                                    onPress={toggleShowPassword} />}
-                            />
-                        )}
-                        name="password"
-                        rules={{ required: 'Campo obrigatório', }}
-                        defaultValue=""
-                    />
+    const handleCloseModal = () => {
+        setModalVisible(false)
+    }
 
-                    <FormControl.ErrorMessage>
-                        {errors.password?.message}
-                    </FormControl.ErrorMessage>
-                </FormControl>
-                {loading ?
-                    <View style={styles.spinnerStyle} mt={6} >
-                        <Spinner color='white' />
-                    </View> :
-                    <Button
-                        onPress={handleSubmit(onSubmit)}
-                        bg={gymbrozTheme.palette.secondary.main}
-                        _pressed={{ bg: gymbrozTheme.palette.secondary.dark }}
-                        mt={6}
-                    >
-                        Entrar
-                    </Button>}
-            </VStack>
-            <Text
-                color={gymbrozTheme.palette.primary.main}
-                mt={10}
-                underline
-                fontSize="md"
-            >
-                Recuperar Acesso
-            </Text>
-            <Text
-                color={gymbrozTheme.palette.primary.main}
-                mt={2}
-                fontSize="md"
-            >
-                Não tem uma conta?
+    return (
+        <ScrollView>
+            <Center flex={1} justifyContent='flex-start' paddingTop={12}>
+                <Text bold fontSize="2xl" style={styles.title}>GymBroz</Text>
+                <Image source={require('../../../assets/img/img-gymbroz.png')} size={'xl'} alt={"Homem forte"} />
+                <VStack width="100%" mx="3" maxW="300px">
+                    <FormControl isInvalid={'email' in errors}>
+                        <FormControl.Label mt={2}>E-mail</FormControl.Label>
+                        <Controller
+                            control={control}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <Input
+                                    onBlur={onBlur}
+                                    placeholder="E-mail"
+                                    onChangeText={(val) => onChange(val)}
+                                    value={value}
+                                    _focus={{
+                                        bg: 'lightBlue.50'
+                                    }}
+                                    autoCapitalize='none'
+                                />
+                            )}
+                            name="email"
+                            rules={{
+                                required: 'Campo obrigatório',
+                                pattern: { value: REGEX_EMAIL, message: 'E-mail inválido' }
+                            }}
+                            defaultValue=""
+                        />
+                        <FormControl.ErrorMessage>
+                            {errors.email?.message}
+                        </FormControl.ErrorMessage>
+                    </FormControl>
+                    <FormControl isInvalid={'password' in errors}>
+                        <FormControl.Label mt={2}>Senha</FormControl.Label>
+                        <Controller
+                            control={control}
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <Input
+                                    onBlur={onBlur}
+                                    secureTextEntry={!showPassword}
+                                    placeholder="Password"
+                                    type="password"
+                                    onChangeText={(val) => onChange(val)}
+                                    value={value}
+                                    _focus={{
+                                        bg: 'lightBlue.50'
+                                    }}
+                                    autoCapitalize='none'
+                                    InputRightElement={<MaterialCommunityIcons
+                                        name={showPassword ? 'eye-off' : 'eye'}
+                                        size={28}
+                                        style={styles.icon}
+                                        onPress={toggleShowPassword} />}
+                                />
+                            )}
+                            name="password"
+                            rules={{ required: 'Campo obrigatório', }}
+                            defaultValue=""
+                        />
+
+                        <FormControl.ErrorMessage>
+                            {errors.password?.message}
+                        </FormControl.ErrorMessage>
+                    </FormControl>
+                    {loading ?
+                        <View style={styles.spinnerStyle} mt={6} >
+                            <Spinner color='white' />
+                        </View> :
+                        <Button
+                            onPress={handleSubmit(onSubmit)}
+                            bg={gymbrozTheme.palette.secondary.main}
+                            _pressed={{ bg: gymbrozTheme.palette.secondary.dark }}
+                            mt={6}
+                        >
+                            Entrar
+                        </Button>}
+                </VStack>
                 <Text
                     color={gymbrozTheme.palette.primary.main}
+                    mt={10}
                     underline
                     fontSize="md"
                 >
-                    Cadastre-se
+                    Recuperar Acesso
                 </Text>
-            </Text>
-        </Center>
+                <Text
+                    color={gymbrozTheme.palette.primary.main}
+                    mt={2}
+                    fontSize="md"
+                    
+                >
+                    Não tem uma conta?
+                    <Text
+                        color={gymbrozTheme.palette.primary.main}
+                        underline
+                        fontSize="md"
+                        onPress={() => setModalVisible(true)}
+                    >
+                    Cadastre-se
+                    </Text>
+                </Text>
+                <ModalSignUp showModal={modalVisible} closeModal={handleCloseModal} />
+            </Center>
+        </ScrollView>
     )
 };
 
