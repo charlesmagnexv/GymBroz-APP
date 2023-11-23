ac3745ef6c1d03cd9f286857d57995ed6208c14b
joaumsud <joaumsud@hotmail.com>
2023-11-22 22:58:46 -0300

    Inserindo os componentes no APP.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import 'react-native-gesture-handler';
-import { StyleSheet, } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import Routes from './src/routes';
-import { AuthProvider } from './src/context/auth';
-import { NativeBaseProvider } from 'native-base';
-import { FeedbackProvider } from './src/context/useFeedback';
+import "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import Routes from "./src/routes";
+import { AuthProvider } from "./src/context/auth";
+import { NativeBaseProvider } from "native-base";
+import { FeedbackProvider } from "./src/context/useFeedback";
+import PasswordRecoveryScreen from "./src/screens/PasswordRecoveryScreen";
+import RecoverySuccessModal from "./src/components/RecoverySuccessModal";
 
 export default function App() {
   return (
@@ -14,8 +16,10 @@ export default function App() {
         <NavigationContainer>
           <AuthProvider>
             <Routes />
+            <PasswordRecoveryScreen />
+            <RecoverySuccessModal />
           </AuthProvider>
-        </NavigationContainer >
+        </NavigationContainer>
       </FeedbackProvider>
     </NativeBaseProvider>
   );
@@ -24,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
